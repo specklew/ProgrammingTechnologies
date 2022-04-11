2c96e0368521d782551cb6079ce5592183f5762a
HaMaKac <50404753+HaMaKac@users.noreply.github.com>
2022-04-11 18:59:04 +0200

    CRUD in LogicLayerAPI

--- MusicShop/Logic/LogicLayerAPI.cs
+++ MusicShop/Logic/LogicLayerAPI.cs
@@ -24,5 +24,45 @@ namespace MusicShop.Logic
 
         ^All of these methods must be abstract here and inherited in DataService.cs
          */
+
+        //Create
+
+        public abstract IUser CreateCustomer(string name, int age);
+
+        public abstract void CreateProduct(string name, string description, float price);
+        
+        public abstract IState CreateState(int stateId, ProductCatalog catalog);
+
+        public abstract IOrder CreateOrder(Product product, int quantity);
+        
+        public abstract Event CreateEvent(IUser user, IOrder order, OrderStatus status, IState state);
+
+        //Read
+
+        public abstract IUser GetUser(string guid);
+
+        public abstract Product GetProduct(string name);
+
+        public abstract Event GetEvent(string guid);
+
+        public abstract Event GetEvent(IUser user, IState state);
+
+        public abstract IState GetState(int stateId);
+
+        //Update
+
+        public abstract void UpdateCustomer(string guid, string name, int age);
+
+        public abstract void UpdateProduct(string name, string description, float price);
+
+        public abstract void UpdateOrder(Product product, int quantity);
+
+        //Delete
+
+        public abstract void DeleteCustomer(string guid);
+
+        public abstract void DeleteProduct(string name);
+
+        public abstract void DeleteOrder(int guid);
     }
 }
